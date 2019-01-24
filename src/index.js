6b21a4dee01a50779e20b95ac3d2aff0d7c4aec6
thalvari <tuomas.halvari@helsinki.fi>
2019-01-24 14:57:12 +0200

    -

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => (
+    <div>
+        <h1>{props.course}</h1>
+    </div>
+)
+
+const Content = (props) => (
+    <div>
+        <p>
+            {props.part1} {props.exercises1}
+        </p>
+        <p>
+            {props.part2} {props.exercises2}
+        </p>
+        <p>
+            {props.part3} {props.exercises3}
+        </p>
+    </div>
+)
+
+const Total = (props) => (
+    <div>
+        <p>yhteensä {props.total} tehtävää</p>
+    </div>
+)
+
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
     const part1 = 'Reactin perusteet'
@@ -12,17 +38,10 @@ const App = () => {
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>yhteensä {exercises1 + exercises2 + exercises3} tehtävää</p>
+            <Header course={course}/>
+            <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3}
+                     exercises3={exercises3}/>
+            <Total total={exercises1 + exercises2 + exercises3}/>
         </div>
     )
 }
